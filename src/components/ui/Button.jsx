7b2ed527ecfd658d2bdc83f553a82bc45048014f
Sergio Guzmán <serguz95444@gmail.com>
2026-04-08 22:36:51 -0600

    fix: correct font name and adjust border radius values in CSS

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -3,10 +3,12 @@ import { Link } from "react-router-dom"
 export default function Button({ link, children }) {
 	return (
 
-		<Link to={link} className="flex min-w-50 cursor-pointer items-center justify-center rounded-lg h-14 px-8 bg-primary text-white text-lg font-bold tracking-wide hover:bg-primary/90 transition-all">
-			<span>
-				{children}
-			</span>
+		<Link to={link} >
+			<button className="flex min-w-50 cursor-pointer items-center justify-center rounded-lg h-14 px-8 bg-primary text-white text-lg font-bold tracking-wide hover:bg-primary/90 transition-all">
+				<span>
+					{children}
+				</span>
+			</button>
 		</Link>
 	)
 }
